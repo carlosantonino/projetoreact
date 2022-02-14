1598341916afaab102f40815468c3bc7e7119117
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-02-13 22:00:13 -0300

    Criando components Botao e Cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 function App() {
   const curso = "React";
