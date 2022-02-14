c3cd6fe882e2ca0c6bab73646956b8999b81d6bb
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-02-13 21:46:40 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
-class App extends Component {
-  render() {
+function App() {
+  const curso = "React";
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <h1>Olá { curso }</h1>
+        <p>Menu primeiro App</p>
       </div>
     );
-  }
+  
 }
 
 export default App;
